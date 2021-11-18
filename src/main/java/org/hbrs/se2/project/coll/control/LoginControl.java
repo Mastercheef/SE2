17df8499ebd1f088f9bcb444940901365636c4f8
Pascal Jansen <Pascal.Jansen@gmx.net>
2021-11-18 17:00:00 +0100

    Moving LoginControl into Control Package

--- src/main/java/org/hbrs/se2/project/coll/control/LoginControl.java
+++ src/main/java/org/hbrs/se2/project/coll/control/LoginControl.java
@@ -1,4 +1,4 @@
-package org.hbrs.se2.project.coll.control.Login;
+package org.hbrs.se2.project.coll.control;
 
 import org.hbrs.se2.project.coll.dao.UserDAO;
 import org.hbrs.se2.project.coll.dtos.LoginResultDTO;
