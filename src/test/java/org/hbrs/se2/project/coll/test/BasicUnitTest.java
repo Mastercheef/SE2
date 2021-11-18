5510d46a4d8fd58c2eda2760beea9170fd453966
lmicha <32817258+lmicha@users.noreply.github.com>
2021-11-18 16:48:47 +0100

    created test branch + first test

---
+++ src/test/java/org/hbrs/se2/project/coll/test/BasicUnitTest.java
@@ -0,0 +1,14 @@
+package org.hbrs.se2.project.coll.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicUnitTest {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+
+}
