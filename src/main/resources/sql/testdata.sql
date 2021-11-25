6d1d1864d8976b86c015d9788d53e4b6cfc014d7
Frederick Behringer <frederick.behringer@smail.inf.h-brs.de>
2021-11-25 17:10:33 +0100

    New Testdata added

--- src/main/resources/sql/testdata.sql
+++ src/main/resources/sql/testdata.sql
@@ -1,6 +1,9 @@
 insert into col_tab_address
-    values (10000000, 53757, 'Sankt Augustin', 'Deutschland', 'Hofgartenstrasse', 15);
+values (10000000, 53757, 'Sankt Augustin', 'Deutschland', 'Hofgartenstrasse', 15);
 
 insert into col_tab_company
 values ('Nur ein Test...', default, 'https://test.com/awesome', 012398489, 'Erfolgreiche Test Firma GmbH',
         (SELECT MAX(address_id) FROM col_tab_address));
+
+insert into col_tab_user
+    values(to_date('05 Dec 2000', 'DD Mon YYYY'), 'password', '192345', (SELECT MAX(address_id) FROM col_tab_address), 'Herr', 'Professor', 'Frederick', 'Behringer', 'mail', default);
