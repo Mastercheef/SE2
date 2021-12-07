26f1f0c6de3db0cf58b9f171c7de0251a9b348f1
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-07 12:08:00 +0100

    created StudentUserRepositoryTest

---
+++ src/test/java/org/hbrs/se2/project/coll/repository/StudentUserRepositoryTest.java
@@ -0,0 +1,34 @@
+package org.hbrs.se2.project.coll.repository;
+
+import org.hbrs.se2.project.coll.dtos.StudentUserDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureTestEntityManager
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class StudentUserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private StudentUserRepository studentUserRepository;
+
+    private StudentUserDTO studentUserDTO;
+
+    @Test
+    void findStudentUserById() {
+        studentUserDTO = studentUserRepository.findStudentUserById(20000017);
+        assertEquals("muster@muster.de" ,studentUserDTO.getEmail());
+    }
+}
