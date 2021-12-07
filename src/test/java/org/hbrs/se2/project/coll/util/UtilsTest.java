d8ff36920efac68fd027740d07162f46dbb67e91
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-07 10:16:17 +0100

    refine UtilsTest

--- src/test/java/org/hbrs/se2/project/coll/util/UtilsTest.java
+++ src/test/java/org/hbrs/se2/project/coll/util/UtilsTest.java
@@ -9,23 +9,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class UtilsTest {
 
     @Test
-    void append() {
-        Integer[] numberArray = {1,2,3};
-        Integer[] numberArrayAppended = Utils.append(numberArray,4);
-        assertEquals(4,numberArrayAppended[numberArrayAppended.length - 1 ]);
+    void appendLength() {
+        Integer[] numberArray = {1, 2, 3};
+        Integer[] numberArrayAppended = Utils.append(numberArray, 4);
+        assertEquals(numberArray.length + 1, numberArrayAppended.length);
     }
 
     @Test
-    void stringIsEmptyOrNull() {
+    void appendCheckObjectAppended() {
+
+        Integer[] numberArray = {1, 2, 3};
+        Integer[] numberArrayAppended = Utils.append(numberArray, 4);
+        assertEquals(4, numberArrayAppended[numberArrayAppended.length - 1]);
+
+    }
+
+    @Test
+    void stringIsEmpt() {
         String emptyString = "";
         String nullString = null;
         assertTrue(Utils.StringIsEmptyOrNull(emptyString));
-        assertTrue(Utils.StringIsEmptyOrNull(nullString));
+
+    }
+    @Test
+    void stringIsNull() {
+        String nullString = null;
+        assertNull(nullString);
     }
 
     @Test
     void convertToGermanDateFormat() {
-        LocalDate date1 = LocalDate.of(2001,7 , 9);
-        assertEquals("9.7.2001" , Utils.convertToGermanDateFormat(date1));
+        LocalDate date1 = LocalDate.of(2001, 7, 9);
+        assertEquals("9.7.2001", Utils.convertToGermanDateFormat(date1));
     }
 }
