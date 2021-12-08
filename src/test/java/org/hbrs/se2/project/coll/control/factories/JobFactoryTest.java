cd9b08e66541b4fa14852192c056a60d7875e313
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-08 07:18:20 +0100

    Created JobFactoryTest

---
+++ src/test/java/org/hbrs/se2/project/coll/control/factories/JobFactoryTest.java
@@ -0,0 +1,12 @@
+package org.hbrs.se2.project.coll.control.factories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobFactoryTest {
+
+    @Test
+    void createJob() {
+    }
+}
