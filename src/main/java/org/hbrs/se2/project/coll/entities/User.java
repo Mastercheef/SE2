54fec2a4a78e022b6d6a6e17e8594b23d1794582
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-11 20:13:15 +0100

    Lets not compare password

--- src/main/java/org/hbrs/se2/project/coll/entities/User.java
+++ src/main/java/org/hbrs/se2/project/coll/entities/User.java
@@ -142,7 +142,6 @@ public class User {
                 Objects.equals(phone, user.phone) &&
                 Objects.equals(dateOfBirth, user.dateOfBirth) &&
                 Objects.equals(email, user.email) &&
-                Objects.equals(password, user.password) &&
                 Objects.equals(id, user.id);
     }
 
