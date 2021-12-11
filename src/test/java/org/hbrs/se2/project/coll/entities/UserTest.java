258b74aadf4de2f93ac8faceef42c1a1cacdaa4d
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-11 19:52:47 +0100

    Trying to improve equal code coverage

--- src/test/java/org/hbrs/se2/project/coll/entities/UserTest.java
+++ src/test/java/org/hbrs/se2/project/coll/entities/UserTest.java
@@ -11,10 +11,11 @@ class UserTest {
 
     private User userOne = new User();
     private User userTwo = new User();
+    private User userThree = new User();
     private Address address = new Address();
     @BeforeEach
     void setUp() {
-        userOne.setId(199);
+        userOne.setId(197);
         userOne.setSalutation("Herr");
         userOne.setTitle("Dr.");
         userOne.setFirstName("Max");
@@ -31,22 +32,40 @@ class UserTest {
         userOne.setType("st");
         userOne.setPassword("passwd");
 
-        userTwo.setId(199);
+        userTwo.setId(198);
         userTwo.setSalutation("Frau");
         userTwo.setTitle("Dr.");
         userTwo.setFirstName("Maxina");
-        userTwo.setLastName("Mustermann");
+        userTwo.setLastName("Testermann");
+        address.setPostalCode("54321");
+        address.setStreet("Testerstraße");
+        address.setCity("Testerstadt");
+        address.setCountry("Schweiz");
+        address.setHouseNumber("2");
+        userTwo.setAddress(address);
+        userTwo.setPhone("9876543210");
+        userTwo.setDateOfBirth(LocalDate.of(2001,1,1));
+        userTwo.setEmail("maxina@gmx.de");
+        userTwo.setType("!st");
+        userTwo.setPassword("passwd2");
+
+        userOne.setId(199);
+        userOne.setSalutation("Herr");
+        userOne.setTitle("Dr.");
+        userOne.setFirstName("Max");
+        userOne.setLastName("Mustermann");
         address.setPostalCode("12345");
         address.setStreet("Musterstrasse");
         address.setCity("Musterstadt");
         address.setCountry("Deutschland");
         address.setHouseNumber("1");
-        userTwo.setAddress(address);
-        userTwo.setPhone("0123456789");
-        userTwo.setDateOfBirth(LocalDate.of(2000,1,1));
-        userTwo.setEmail("max@gmx.de");
-        userTwo.setType("st");
-        userTwo.setPassword("passwd");
+        userOne.setAddress(address);
+        userOne.setPhone("0123456789");
+        userOne.setDateOfBirth(LocalDate.of(2002,1,1));
+        userOne.setEmail("max@gmx.de");
+        userOne.setType("st");
+        userOne.setPassword("passwd");
+
     }
     @Test
     void testEqualsSameUser() {
@@ -72,7 +91,7 @@ class UserTest {
 
     @Test
     void testToString() {
-        assertEquals("199, Herr, Dr., Max, Mustermann, 2000-01-01, 0123456789, Musterstrasse 1\n" +
+        assertEquals("199, Herr, Dr., Max, Mustermann, 2002-01-01, 0123456789, Musterstrasse 1\n" +
                 "12345 Musterstadt\n" +
                 "Deutschland, max@gmx.de, st", userOne.toString());
     }
