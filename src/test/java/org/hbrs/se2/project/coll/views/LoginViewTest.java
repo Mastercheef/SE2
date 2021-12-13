9b81c9256bb133b4dca48a1c2f89b7ccc8f5b9df
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-13 17:40:55 +0100

    Selenium Test login is working(No idea how but it does)

--- src/test/java/org/hbrs/se2/project/coll/views/LoginViewTest.java
+++ src/test/java/org/hbrs/se2/project/coll/views/LoginViewTest.java
@@ -4,19 +4,23 @@ import com.vaadin.flow.component.button.Button;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
+import java.sql.SQLException;
+
+import static java.sql.DriverManager.getDriver;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginViewTest {
 
 
     @Test
-    public void login() throws InterruptedException
-    {
+    public void login() throws InterruptedException, SQLException {
         // chromedriver
         System.setProperty("webdriver.chrome.driver","src/main/resources/selenium/chromedriver/windows/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -24,16 +28,19 @@ class LoginViewTest {
 
 
         //vaadinlogInusername
-        driver.get("sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login");
+        driver.get("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login");
         Thread.sleep(1000);
         WebElement username=driver.findElement(By.id("vaadinLoginUsername"));
         WebElement password=driver.findElement(By.id("vaadinLoginPassword"));
 
         username.sendKeys("judithmeier@company.com");
         password.sendKeys("abc123");
-        driver.findElement(By.xpath("/html/body/vaadin-vertical-layout/vaadin-login-form/vaadin-login-form-wrapper/form/vaadin-button")).click();
+        WebElement element  = driver.findElement(By.xpath("/html/body/vaadin-vertical-layout/vaadin-login-form/vaadin-login-form-wrapper/form/vaadin-button"));
+        Actions builder = new Actions(driver);
+        builder.moveToElement( element ).click( element );
+        builder.perform();
     }
-
+    //*[@id="forgotPasswordButton"]
     public void loginFirefox() {
         WebDriver driver = new FirefoxDriver();
     }
