865465337d160a07f4ab0843fc4a33b09b1645e2
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-16 12:13:09 +0100

    Replaced local driver with WebDriver

--- src/test/java/org/hbrs/se2/project/coll/views/LoginViewTest.java
+++ src/test/java/org/hbrs/se2/project/coll/views/LoginViewTest.java
@@ -3,22 +3,30 @@ package org.hbrs.se2.project.coll.views;
 import org.hbrs.se2.project.coll.util.Utils;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.sql.SQLException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class LoginViewTest {
 
 
     @Test
-    public void login() throws InterruptedException, SQLException {
+    public void login()  {
 
-        System.setProperty("webdriver.chrome.driver",getLinkToChromeDriver());
-        WebDriver driver = new ChromeDriver();
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver  driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login");
 
@@ -32,17 +40,11 @@ class LoginViewTest {
         builder.moveToElement( element ).click( element );
         builder.perform();
 
+
     }
     public void loginFirefox() {
         WebDriver driver = new FirefoxDriver();
     }
 
-    private String getLinkToChromeDriver() {
-        Utils.OS os = Utils.getOS();
-        if(os.equals(Utils.OS.WINDOWS)) {
-            return "src/main/resources/selenium/chromedriver/windows/chromedriver.exe";
-        } else {
-            return ("src/main/resources/selenium/chromedriver/" +  os.toString().toLowerCase()   +"/chromedriver");
-        }
-    }
+
 }
