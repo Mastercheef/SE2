57ec4f86ae5b5aa48c0a72aa83a26138ac45bd24
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-17 16:57:11 +0100

    Testing LabelCompany

--- src/test/java/org/hbrs/se2/project/coll/util/LabelCompanyTest.java
+++ src/test/java/org/hbrs/se2/project/coll/util/LabelCompanyTest.java
@@ -15,41 +15,59 @@ class LabelCompanyTest {
 
     @Test
     void getStreet() {
+        assertEquals("Strasse:" , labelCompany.getStreet().getText());
     }
 
     @Test
     void getStreetnumber() {
+        assertEquals("Hausnummer:" , labelCompany.getStreetnumber().getText());
     }
 
     @Test
     void getPostalcode() {
+        assertEquals("PLZ:" , labelCompany.getPostalcode().getText());
+
     }
 
     @Test
     void getCity() {
+        assertEquals("Ort:" , labelCompany.getCity().getText());
+
     }
 
     @Test
     void getCountry() {
+        assertEquals("Land:" , labelCompany.getCountry().getText());
+
     }
 
     @Test
     void getEmail() {
+        assertEquals("E-Mail:" , labelCompany.getEmail().getText());
+
     }
 
     @Test
     void getPhone() {
+        assertEquals("Telefon:" , labelCompany.getPhone().getText());
+
     }
 
     @Test
     void getFax() {
+        assertEquals("Fax:" , labelCompany.getFax().getText());
+
     }
 
     @Test
     void getWebsite() {
+        assertEquals("Webseite:" , labelCompany.getWebsite().getText());
+
     }
 
     @Test
     void getDescription() {
+        assertEquals("Beschreibung:" , labelCompany.getDescription().getText());
+
     }
 }
