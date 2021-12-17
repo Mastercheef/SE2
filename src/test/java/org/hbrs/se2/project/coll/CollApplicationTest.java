2ef784c5d25baeab71f8f40a4e52d09c51bb31e9
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-17 16:58:18 +0100

    Testing SpringBootTest

--- src/test/java/org/hbrs/se2/project/coll/CollApplicationTest.java
+++ src/test/java/org/hbrs/se2/project/coll/CollApplicationTest.java
@@ -1,12 +1,12 @@
 package org.hbrs.se2.project.coll;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 // Test class added ONLY to cover main() invocation not covered by application tests.
 class CollApplicationTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void applicationStarts() {
         CollApplication.main(new String[] {});
 
