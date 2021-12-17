dddac8b006cff5dac05d216205288492158f5dab
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-18 00:57:15 +0100

    created Tests for RegistrationResultDTOImpl

---
+++ src/test/java/org/hbrs/se2/project/coll/dtos/impl/RegistrationResultDTOImplTest.java
@@ -0,0 +1,45 @@
+package org.hbrs.se2.project.coll.dtos.impl;
+
+import org.hbrs.se2.project.coll.dtos.RegistrationResultDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationResultDTOImplTest {
+
+    RegistrationResultDTOImpl registrationResultDTO= new RegistrationResultDTOImpl();
+    @BeforeEach
+    void setUp() {
+        registrationResultDTO.addReason(RegistrationResultDTO.ReasonType.CITY_MISSING);
+        registrationResultDTO.addReason(RegistrationResultDTO.ReasonType.COMPANY_CITY_MISSING);
+        registrationResultDTO.addReason(RegistrationResultDTO.ReasonType.COMPANY_WEBSITE_MISSING);
+    }
+    @Test
+    void getResult() {
+        registrationResultDTO.setResult(true);
+        assertTrue(registrationResultDTO.getResult());
+    }
+
+    @Test
+    void getReasons() {
+
+        List<RegistrationResultDTO.ReasonType> list = registrationResultDTO.getReasons();
+        assertEquals(RegistrationResultDTO.ReasonType.CITY_MISSING , list.get(0));
+        assertEquals(RegistrationResultDTO.ReasonType.COMPANY_CITY_MISSING , list.get(1));
+        assertEquals(RegistrationResultDTO.ReasonType.COMPANY_WEBSITE_MISSING , list.get(2));
+
+    }
+
+    @Test
+    void addReason() {
+        assertEquals(registrationResultDTO.getReasons().size() , 3);
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("[CITY_MISSING, COMPANY_CITY_MISSING, COMPANY_WEBSITE_MISSING]" , registrationResultDTO.toString());
+    }
+}
