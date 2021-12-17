eb9c78083f74342dfd05c99343ea8b5099f23418
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-17 16:18:38 +0100

    testing CollApplication

---
+++ src/test/java/org/hbrs/se2/project/coll/CollApplicationTest.java
@@ -0,0 +1,12 @@
+package org.hbrs.se2.project.coll;
+
+
+import org.junit.Test;
+
+
+class CollApplicationTest {
+    @Test
+    public void applicationStarts() {
+        CollApplication.main(new String[] {});
+    }
+}
