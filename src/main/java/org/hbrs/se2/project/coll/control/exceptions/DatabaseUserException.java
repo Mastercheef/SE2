028db9423c567b1d9ca57122d68e7281708b5081
lmicha <lars.michaelis@smail.inf.h-brs.de>
2021-12-18 00:02:34 +0100

    updated DatabaseUserException

--- src/main/java/org/hbrs/se2/project/coll/control/exceptions/DatabaseUserException.java
+++ src/main/java/org/hbrs/se2/project/coll/control/exceptions/DatabaseUserException.java
@@ -1,17 +1,8 @@
 package org.hbrs.se2.project.coll.control.exceptions;
 
 public class DatabaseUserException extends Exception {
-    private String reason = null;
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
 
     public DatabaseUserException( String reason ) {
-        this.reason = reason;
+        super(reason);
     }
 }
