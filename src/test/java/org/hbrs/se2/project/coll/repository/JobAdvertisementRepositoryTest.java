ff161269419bcde3d721c2691c5effedf333c5ca
lm <larsmichaelis@gmx.net>
2021-12-30 17:53:46 +0100

    Added JobAdvertisementRepositoryTest

--- src/test/java/org/hbrs/se2/project/coll/repository/JobAdvertisementRepositoryTest.java
+++ src/test/java/org/hbrs/se2/project/coll/repository/JobAdvertisementRepositoryTest.java
@@ -2,8 +2,11 @@ package org.hbrs.se2.project.coll.repository;
 
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
@@ -11,12 +14,22 @@ import static org.junit.jupiter.api.Assertions.*;
 @Sql( {"/schema.sql" , "/data.sql"})
 class JobAdvertisementRepositoryTest {
 
+    @Autowired
+    JobAdvertisementRepository jobAdvertisementRepository;
+
     @Test
     void findJobAdvertisementById() {
+        assertNotNull(        jobAdvertisementRepository.findJobAdvertisementById(30000000));
+    }
+
+    @Test
+    void findJobAdvertisementByIde() {
+        assertEquals(     10,   jobAdvertisementRepository.findJobAdvertisementById(30000000).getSalary());
     }
 
     @Test
     void filterJobs() {
+        assertNotNull(        jobAdvertisementRepository.filterJobs("","","" ,true , LocalDate.of(2000,1,2) ,(short)40 ,2999));
     }
 
     @Test
