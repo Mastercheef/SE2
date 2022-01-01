5ac037daff90f00bf76c0eb2cf9c5921915f0d56
lm <larsmichaelis@gmx.net>
2022-01-01 10:37:32 +0100

    Added Tests for DatabaseUserExceptionTest

--- src/test/java/org/hbrs/se2/project/coll/control/exceptions/DatabaseUserExceptionTest.java
+++ src/test/java/org/hbrs/se2/project/coll/control/exceptions/DatabaseUserExceptionTest.java
@@ -1,6 +1,35 @@
 package org.hbrs.se2.project.coll.control.exceptions;
 
+import org.hbrs.se2.project.coll.dtos.RegistrationResultDTO;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseUserExceptionTest {
+
+    private static final String MESSAGE = "Wrong User";
+
+    @Test
+    void registrationResultMessage() {
+        DatabaseUserException databaseUserException = new DatabaseUserException(MESSAGE);
+        assertEquals(MESSAGE , databaseUserException.getMessage());
+        assertEquals(databaseUserException.getErrorCode() , RegistrationResultDTO.ReasonType.DATABASE_USER_EXCEPTION);
+    }
+
+    @Test
+    void registrationResultMesageThrowable() {
+        DatabaseUserException databaseUserException = new DatabaseUserException(MESSAGE, new Throwable());
+        assertEquals(MESSAGE , databaseUserException.getMessage());
+        assertNotNull(databaseUserException.getCause());
+        assertEquals(databaseUserException.getErrorCode() , RegistrationResultDTO.ReasonType.DATABASE_USER_EXCEPTION);
+    }
+    @Test
+    void registrationResultMesageThrowableRegistrationResult() {
+        DatabaseUserException databaseUserException = new DatabaseUserException(MESSAGE, new Throwable() , RegistrationResultDTO.ReasonType.CITY_MISSING);
+        assertEquals(MESSAGE , databaseUserException.getMessage());
+        assertNotNull(databaseUserException.getCause());
+        assertEquals(databaseUserException.getErrorCode() , RegistrationResultDTO.ReasonType.CITY_MISSING);
+    }
+
+
 }
