413e25b628e356d1c1295f78207f68fc541b99cb
lm <larsmichaelis@gmx.net>
2022-01-04 18:00:52 +0100

    testing loadStudentUserInfo

--- src/test/java/org/hbrs/se2/project/coll/util/JobApplicationFormularUtilTest.java
+++ src/test/java/org/hbrs/se2/project/coll/util/JobApplicationFormularUtilTest.java
@@ -6,20 +6,52 @@ import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import org.hbrs.se2.project.coll.dtos.JobApplicationResultDTO;
 import org.hbrs.se2.project.coll.dtos.RegistrationResultDTO;
+import org.hbrs.se2.project.coll.entities.Address;
+import org.hbrs.se2.project.coll.entities.StudentUser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
 class JobApplicationFormularUtilTest {
 
     JobApplicationFormularUtil jobApplicationFormularUtil;
 
     private String error = "error";
     private String header = "Header";
+    private String address = "address";
+    private String headLine = "Bewerbung";
+    private String name = "sname";
+    private LocalDate localDate = LocalDate.of(2000,1,2);
+    private String email = "email";
+    private String country = "Country";
+    private String phone = "phone";
+    private String location = "location";
+    private String salutation = "Salutation";
+    private String title = "Title";
+    private String firstName = "Max";
+    private String lastName = "Mustermann";
+    private String street = "Musterstraße";
+    private String houseNumber = "2";
+    private String postalCode = "12345";
+    private String city = "Musterstadt";
+
+
+    @Mock
+    StudentUser studentUser;
+
+    @Mock
+    Address addressEntity;
 
     @BeforeEach
     void setup() {
@@ -40,7 +72,7 @@ class JobApplicationFormularUtilTest {
 
     @Test
     void getPageHeadline() {
-        assertEquals("Bewerbung" , jobApplicationFormularUtil.getPageHeadline().getText());
+        assertEquals( headLine , jobApplicationFormularUtil.getPageHeadline().getText());
         jobApplicationFormularUtil.setPageHeadline(new H2(header));
         assertEquals(header , jobApplicationFormularUtil.getPageHeadline().getText());
     }
@@ -58,7 +90,7 @@ class JobApplicationFormularUtilTest {
     @Test
     void setsSalutation() {
         assertNotNull(jobApplicationFormularUtil.getsSalutation());
-        jobApplicationFormularUtil.setsSalutation(new Span("Salutation"));
+        jobApplicationFormularUtil.setsSalutation(new Span(salutation));
         assertNotNull(jobApplicationFormularUtil.getsSalutation());
 
     }
@@ -70,7 +102,6 @@ class JobApplicationFormularUtilTest {
 
     @Test
     void setsName() {
-        String name = "sname";
         jobApplicationFormularUtil.setsName(new Span(name));
         assertNotNull(jobApplicationFormularUtil.getsName());
         assertEquals(name , jobApplicationFormularUtil.getsName().getElement().getText());
@@ -83,7 +114,7 @@ class JobApplicationFormularUtilTest {
 
     @Test
     void setsDateOfBirth() {
-        LocalDate localDate = LocalDate.of(2000,1,2);
+
         jobApplicationFormularUtil.setsDateOfBirth(new Span(localDate.toString()));
         assertNotNull(jobApplicationFormularUtil.getsDateOfBirth());
         assertEquals(localDate.toString() , jobApplicationFormularUtil.getsDateOfBirth().getElement().getText());
@@ -91,7 +122,7 @@ class JobApplicationFormularUtilTest {
 
     @Test
     void getsEmail() {
-        String email = "email";
+
         jobApplicationFormularUtil.setsEmail(new Span(email));
         assertNotNull(jobApplicationFormularUtil.getsEmail());
         assertEquals(email , jobApplicationFormularUtil.getsEmail().getElement().getText());
@@ -105,7 +136,7 @@ class JobApplicationFormularUtilTest {
     @Test
     void getsPhone() {
 
-        String phone = "phone";
+
         jobApplicationFormularUtil.setsPhone(new Span(phone));
         assertEquals(phone , jobApplicationFormularUtil.getsPhone().getElement().getText());
     }
@@ -118,7 +149,7 @@ class JobApplicationFormularUtilTest {
 
     @Test
     void getsAddress() {
-        String address = "address";
+
         jobApplicationFormularUtil.setsAddress(new Span(address));
         assertNotNull(jobApplicationFormularUtil.getsAddress());
         assertEquals(address , jobApplicationFormularUtil.getsAddress().getElement().getText());
@@ -132,7 +163,7 @@ class JobApplicationFormularUtilTest {
 
     @Test
     void getsLocation() {
-        String location = "location";
+
         jobApplicationFormularUtil.setsLocation(new Span(location));
         assertNotNull(jobApplicationFormularUtil.getsLocation());
         assertEquals(location , jobApplicationFormularUtil.getsLocation().getElement().getText());
@@ -147,7 +178,6 @@ class JobApplicationFormularUtilTest {
 
     @Test
     void getsCountry() {
-        String country = "Country";
         jobApplicationFormularUtil.setsCountry(new Span(country));
         assertNotNull(jobApplicationFormularUtil.getsCountry());
         assertEquals(country , jobApplicationFormularUtil.getsCountry().getElement().getText());
@@ -168,4 +198,58 @@ class JobApplicationFormularUtilTest {
         jobApplicationFormularUtil.setErrorFields(arrayList , new TextField() , new TextArea());
         assertEquals("\"Fehler\"" , jobApplicationFormularUtil.getError());
     }
+
+
+    @Test
+    void loadStudentUserInfo(){
+
+        studentUser.setAddress(addressEntity);
+
+        mockLoadStudentUser();
+
+        jobApplicationFormularUtil.loadStudentUserInfo(studentUser);
+
+        assertNotNull(jobApplicationFormularUtil.getsSalutation());
+        assertEquals(salutation + " " + title , jobApplicationFormularUtil.getsSalutation().getElement().getText());
+        assertNotNull(jobApplicationFormularUtil.getsName());
+        assertEquals(firstName + " " + lastName , jobApplicationFormularUtil.getsName().getElement().getText());
+        assertNotNull(jobApplicationFormularUtil.getsDateOfBirth());
+        assertEquals(Utils.convertToGermanDateFormat(localDate) , jobApplicationFormularUtil.getsDateOfBirth().getElement().getText());
+        assertNotNull(jobApplicationFormularUtil.getsEmail());
+        assertEquals(email , jobApplicationFormularUtil.getsEmail().getElement().getText());
+        assertNotNull(jobApplicationFormularUtil.getsPhone());
+        assertEquals(phone , jobApplicationFormularUtil.getsPhone().getElement().getText());
+        assertNotNull(jobApplicationFormularUtil.getsAddress());
+        assertEquals(street + " " + houseNumber , jobApplicationFormularUtil.getsAddress().getElement().getText());
+        assertNotNull(jobApplicationFormularUtil.getsLocation());
+        assertEquals (postalCode + " " + city , jobApplicationFormularUtil.getsLocation().getElement().getText());
+        assertNotNull(jobApplicationFormularUtil.getsCountry());
+        assertEquals(country , jobApplicationFormularUtil.getsCountry().getElement().getText());
+
+    }
+
+    private void mockLoadStudentUser() {
+        studentUser.setAddress(addressEntity);
+
+        when(studentUser.getAddress()).thenReturn(addressEntity);
+        when(studentUser.getSalutation()).thenReturn(salutation);
+        when(studentUser.getTitle()).thenReturn(title);
+        when(studentUser.getFirstName()).thenReturn(firstName);
+        when(studentUser.getLastName()).thenReturn(lastName);
+        when(studentUser.getEmail()).thenReturn(email);
+        when(studentUser.getPhone()).thenReturn(phone);
+        when(studentUser.getDateOfBirth()).thenReturn(localDate);
+
+
+        when(studentUser.getAddress().getHouseNumber()).thenReturn(houseNumber);
+        when(studentUser.getAddress().getPostalCode()).thenReturn(postalCode);
+        when(studentUser.getAddress().getStreet()).thenReturn(street);
+        when(studentUser.getAddress().getCity()).thenReturn(city);
+        when(studentUser.getAddress().getCountry()).thenReturn(country);
+    }
+
+
+
+
+
 }
