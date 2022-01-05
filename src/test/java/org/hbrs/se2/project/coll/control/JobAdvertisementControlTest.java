a877b8169c6d8e0b991cb75bdf187695e0b32836
lm <larsmichaelis@gmx.net>
2022-01-05 09:09:41 +0100

    covering all conditions

--- src/test/java/org/hbrs/se2/project/coll/control/JobAdvertisementControlTest.java
+++ src/test/java/org/hbrs/se2/project/coll/control/JobAdvertisementControlTest.java
@@ -1,6 +1,7 @@
 package org.hbrs.se2.project.coll.control;
 
 import org.hbrs.se2.project.coll.dtos.JobAdvertisementDTO;
+import org.hbrs.se2.project.coll.entities.Address;
 import org.hbrs.se2.project.coll.entities.JobAdvertisement;
 import org.hbrs.se2.project.coll.repository.CompanyRepository;
 import org.hbrs.se2.project.coll.repository.ContactPersonRepository;
@@ -31,6 +32,8 @@ class JobAdvertisementControlTest {
     @Mock
     JobAdvertisement jobAdvertisement;
 
+    @Mock
+    Address addressReturn;
 
 
     @Test
@@ -40,56 +43,4 @@ class JobAdvertisementControlTest {
         assertEquals(jobAdvertisement, jobAdvertisementControl.getJob(100));
     }
 
-    @Test
-    void createJobDTO() {
-
-    }
-
-    @Test
-    void getCompanyName() {
-    }
-
-    @Test
-    void getCompanyAddress() {
-    }
-
-    @Test
-    void getCompanyPhoneNumber() {
-    }
-
-    @Test
-    void getContactPersonName() {
-    }
-
-    @Test
-    void getContactPersonEmail() {
-    }
-
-    @Test
-    void getCompanyId() {
-    }
-
-    @Test
-    void getAllJobs() {
-    }
-
-    @Test
-    void filterJobs() {
-    }
-
-    @Test
-    void testFilterJobs() {
-    }
-
-    @Test
-    void testFilterJobs1() {
-    }
-
-    @Test
-    void testFilterJobs2() {
-    }
-
-    @Test
-    void filterCompanies() {
-    }
 }
