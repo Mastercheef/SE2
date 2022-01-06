8ea8e40bdc3efea3229178a50565f2e46a68e602
lm <larsmichaelis@gmx.net>
2022-01-07 00:45:52 +0100

    tested prepreareSending

--- src/test/java/org/hbrs/se2/project/coll/control/InboxControlTest.java
+++ src/test/java/org/hbrs/se2/project/coll/control/InboxControlTest.java
@@ -1,6 +1,7 @@
 package org.hbrs.se2.project.coll.control;
 
 import org.hbrs.se2.project.coll.dtos.MessageDTO;
+import org.hbrs.se2.project.coll.dtos.impl.MessageDTOImpl;
 import org.hbrs.se2.project.coll.repository.ContactPersonRepository;
 import org.hbrs.se2.project.coll.repository.JobAdvertisementRepository;
 import org.hbrs.se2.project.coll.repository.MessageRepository;
@@ -10,12 +11,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class InboxControlTest {
 
     @InjectMocks
@@ -35,6 +40,12 @@ class InboxControlTest {
 
     @Mock
     MessageDTO messageDTO;
+
+    MessageDTO messageDTOMethod;
+
+    private String message = "message";
+    private String subbject = "Subject";
+    private LocalDate date = LocalDate.of(2000,1 ,12);
     @Test
     void getMessages() {
         ArrayList<MessageDTO> messageDTOArrayList = new ArrayList<>();
@@ -43,5 +54,25 @@ class InboxControlTest {
         assertEquals(1 , inboxControl.getMessages(100).size());
         messageDTOArrayList.add(messageDTO);
         assertEquals(2 , inboxControl.getMessages(100).size());
+    }
+
+    @Test
+    void prepareSending() {
+        when(messageDTO.getRecipient()).thenReturn(200);
+        when(messageDTO.getSender()).thenReturn(400);
+        when(messageDTO.getSubject()).thenReturn(subbject);
+        when(messageDTO.getDate()).thenReturn(date);
+
+        messageDTOMethod = inboxControl.prepareSending(messageDTO,message);
+
+        assertEquals(message , messageDTOMethod.getContent());
+        assertEquals(200 , messageDTOMethod.getRecipient());
+        assertEquals(400 , messageDTOMethod.getSender());
+        assertEquals(subbject , messageDTOMethod.getSubject());
+        assertEquals(message , messageDTOMethod.getContent());
+        assertEquals(LocalDate.now() , messageDTOMethod.getDate());
+
+
+
     }
 }
