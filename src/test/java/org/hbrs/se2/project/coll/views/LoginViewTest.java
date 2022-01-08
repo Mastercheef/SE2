746e461d3324600b177a66917fc9b272c0318d09
lm <larsmichaelis@gmx.net>
2022-01-08 22:19:20 +0100

    chromedriver login for LoginView

--- src/test/java/org/hbrs/se2/project/coll/views/LoginViewTest.java
+++ src/test/java/org/hbrs/se2/project/coll/views/LoginViewTest.java
@@ -1,10 +1,15 @@
 package org.hbrs.se2.project.coll.views;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -19,75 +24,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class LoginViewTest {
 
 
-    @Disabled
-    @Test
-    public synchronized  void login()  {
+    WebDriver driver;
 
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @ValueSource(classes = { ChromeDriver.class })
+    void test(Class<? extends WebDriver> webDriverClass)   {
+
+        driver = WebDriverManager.getInstance(webDriverClass).create();
         driver.manage().window().maximize();
         driver.get("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login");
-
         WebElement username=driver.findElement(By.id("vaadinLoginUsername"));
         WebElement password=driver.findElement(By.id("vaadinLoginPassword"));
-        username.sendKeys("www@gmx.de");
-        password.sendKeys("www123");
+        username.sendKeys("olaf@gmbh.de");
+        password.sendKeys("passwd");
         WebElement element  = driver.findElement(By.xpath("/html/body/vaadin-vertical-layout/vaadin-login-form/vaadin-login-form-wrapper/form/vaadin-button"));
-
         Actions builder = new Actions(driver);
         builder.moveToElement( element ).click( element );
         builder.perform();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.xpath("/html/body/vaadin-app-layout/vaadin-vertical-layout/vaadin-vertical-layout/vaadin-vertical-layout/img"));
         assertEquals("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/" , driver.getCurrentUrl());
-
-    }
-
-    @Disabled
-    @Test
-    public synchronized  void loginFireFox()  {
-
-        WebDriverManager.edgedriver().setup();
-        FirefoxDriver driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.get("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login");
-
-        WebElement username=driver.findElement(By.id("vaadinLoginUsername"));
-        WebElement password=driver.findElement(By.id("vaadinLoginPassword"));
-        username.sendKeys("www@gmx.de");
-        password.sendKeys("www123");
-        WebElement element  = driver.findElement(By.xpath("/html/body/vaadin-vertical-layout/vaadin-login-form/vaadin-login-form-wrapper/form/vaadin-button"));
-
-        Actions builder = new Actions(driver);
-        builder.moveToElement( element ).click( element );
-        builder.perform();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("/html/body/vaadin-app-layout/vaadin-vertical-layout/vaadin-vertical-layout/vaadin-vertical-layout/img"));
-        assertEquals("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/" , driver.getCurrentUrl());
-
-    }
-
-    @Disabled
-    @Test
-    public   void loginFail()  {
-
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login");
-
-        WebElement username=driver.findElement(By.id("vaadinLoginUsername"));
-        WebElement password=driver.findElement(By.id("vaadinLoginPassword"));
-        username.sendKeys("www@gmx.net");
-        password.sendKeys("www123");
-        WebElement element  = driver.findElement(By.xpath("/html/body/vaadin-vertical-layout/vaadin-login-form/vaadin-login-form-wrapper/form/vaadin-button"));
-
-        Actions builder = new Actions(driver);
-        builder.moveToElement( element ).click( element );
-        builder.perform();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//*[@id=\"button\"]"));
-        assertEquals("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login" , driver.getCurrentUrl());
 
     }
 
