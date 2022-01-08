4588274f2584da03615282791307d5b378281f0d
lm <larsmichaelis@gmx.net>
2022-01-08 22:32:09 +0100

    LoginView

--- src/test/java/org/hbrs/se2/project/coll/views/LoginViewTest.java
+++ src/test/java/org/hbrs/se2/project/coll/views/LoginViewTest.java
@@ -12,8 +12,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.concurrent.TimeUnit;
 
@@ -34,12 +39,12 @@ class LoginViewTest {
 
     @Disabled
     @ParameterizedTest
-    @ValueSource(classes = { ChromeDriver.class })
+    @ValueSource(classes = { ChromeDriver.class , EdgeDriver.class , OperaDriver.class , FirefoxDriver.class , SafariDriver.class})
     void test(Class<? extends WebDriver> webDriverClass)   {
 
         driver = WebDriverManager.getInstance(webDriverClass).create();
         driver.manage().window().maximize();
-        driver.get("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login");
+        driver.get("htt://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/login");
         WebElement username=driver.findElement(By.id("vaadinLoginUsername"));
         WebElement password=driver.findElement(By.id("vaadinLoginPassword"));
         username.sendKeys("olaf@gmbh.de");
@@ -53,5 +58,28 @@ class LoginViewTest {
         assertEquals("http://sepp-test.inf.h-brs.de:8080/Team_ALT-F4/" , driver.getCurrentUrl());
 
     }
+
+    @Disabled
+    @ParameterizedTest
+    @ValueSource(classes = { ChromeDriver.class , EdgeDriver.class , OperaDriver.class , FirefoxDriver.class , SafariDriver.class})
+    void testLocalHost(Class<? extends WebDriver> webDriverClass)   {
+
+        driver = WebDriverManager.getInstance(webDriverClass).create();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:8080/login/");
+        WebElement username=driver.findElement(By.id("vaadinLoginUsername"));
+        WebElement password=driver.findElement(By.id("vaadinLoginPassword"));
+        username.sendKeys("olaf@gmbh.de");
+        password.sendKeys("passwd");
+        WebElement element  = driver.findElement(By.xpath("/html/body/vaadin-vertical-layout/vaadin-login-form/vaadin-login-form-wrapper/form/vaadin-button"));
+        Actions builder = new Actions(driver);
+        builder.moveToElement( element ).click( element );
+        builder.perform();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body/vaadin-app-layout/vaadin-vertical-layout/vaadin-vertical-layout/vaadin-vertical-layout/img"));
+        assertEquals("http://localhost:8080/" , driver.getCurrentUrl());
+
+    }
+
 
 }
